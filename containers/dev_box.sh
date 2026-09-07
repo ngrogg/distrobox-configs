4f0ba6fabc78c5db77cee6e500ab4bec9571140c
Nicholas Grogg <Nicholas_Grogg6505@outlook.com>
2026-09-07 12:50:03 +0000

    containers/dev_box.sh. Initial draft of script for configuring dev environment.

--- containers/dev_box.sh
+++ containers/dev_box.sh
@@ -11,3 +11,38 @@ set -e
 # set -u
 # Uncomment for exit on non-zero status from rightmost pipe command
 # set -o pipefail
+
+# Create container
+distrobox create --image fedora:latest --name dev_box
+
+# Update container
+distrobox enter dev_environment -- sudo dnf update -y
+
+# Install packages on container, change as needed
+distrobox enter dev_environment -- sudo dnf install -y \
+    ansible \
+    cmake \
+    gcc-c++ \
+    git \
+    ksshaskpass \
+    make \
+    perl-JSON \
+    perl-XML-LibXML \
+    perl-XML-Simple \
+    python3 \
+    python3-devel \
+    python3-pip \
+    vim-enhanced \
+    wl-clipboard \
+    zsh
+
+# Download/place zsh/vim rc files
+distrobox enter dev_box -- wget -O .zshrc https://raw.githubusercontent.com/ngrogg/dotfiles/refs/heads/main/.zshrc
+distrobox enter dev_box -- wget -O .vimrc https://raw.githubusercontent.com/ngrogg/dotfiles/refs/heads/main/.vimrc.simple
+
+# Create related vim/zsh directories
+distrobox enter dev_box -- mkdir -p .zsh/cache
+distrobox enter dev_box -- mkdir -p .vim/backupdir
+
+# Set container shell
+distrobox enter dev_box -- chsh -s $(which zsh)
