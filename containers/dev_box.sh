1bfac188376be09115f5c3dae41fb1d0c809c2b9
ngrogg <Nicholas_Grogg6505@outlook.com>
2026-09-19 16:33:18 -1000

    containers/dev_box.sh. Readded gitconfig section.

--- containers/dev_box.sh
+++ containers/dev_box.sh
@@ -10,6 +10,13 @@ set -e
 # Uncomment for error on unset variables
 set -u
 
+# Variables
+## Name for gitconfig
+local gitUsername="$1"
+
+## Email for gitconfig
+local gitEmail="$2"
+
 # Create container
 distrobox-create --image fedora:latest --name dev_box
 
@@ -21,6 +28,7 @@ distrobox-enter dev_box -- sudo dnf install -y \
     ansible \
     cmake \
     gcc-c++ \
+    git \
     make \
     perl-JSON \
     perl-XML-LibXML \
@@ -40,6 +48,20 @@ distrobox-enter dev_box -- wget -O .zshrc https://raw.githubusercontent.com/ngro
 # Create related vim/zsh directories
 distrobox-enter dev_box -- mkdir -p .zsh/cache
 distrobox-enter dev_box -- mkdir -p .vim/backupdir
+
+# Set gitconfig
+## Variable checks
+if [[ -z "$gitUsername" ]]; then
+    read -p "Enter a username for git commits" : gitUsername
+fi
+
+if [[ -z "$gitEmail" ]]; then
+    read -p "Enter an email for git commits" : gitEmail
+fi
+
+## Set git configs
+distrobox-enter dev_box -- git config --global user.name "$gitUsername"
+distrobox-enter dev_box -- git config --global user.email "$gitEmail"
 
 # Set container shell
 distrobox-enter dev_box -- chsh -s /usr/bin/zsh
